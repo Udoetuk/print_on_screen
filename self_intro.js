230c4014e730ef0746b85892632b5731dc111fb6
UdoetukEkemini <udoetukekemini@gmail.com>
2022-06-24 13:04:58 +0100

    first commit

---
+++ self_intro.js
@@ -0,0 +1,7 @@
+let person = {
+    name: "Ekemini Udoetuk",
+    height: "6 Feet 3 inches",
+    country: "Nigeria"
+}
+
+document.getElementById("demo").innerHTML= `My name is ${person.name} and i am ${person.height} tall, and i am from ${person.country}.`
